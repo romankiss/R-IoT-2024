a6e24d0395995e184833174dc1cdb4586824be2d
Roman Kiss <30365471+romankiss@users.noreply.github.com>
2024-09-11 11:27:41 -0700

    Add files via upload

---
+++ Students/abc/Projects/ABC/Program.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Diagnostics;
+using System.Threading;
+
+namespace NFApp02
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            Debug.WriteLine("Hello from nanoFramework!");
+
+            Thread.Sleep(Timeout.Infinite);
+
+            //// 123 456 789
+            // Browse our samples repository: https://github.com/nanoframework/samples
+            // Check our documentation online: https://docs.nanoframework.net/
+            // Join our lively Discord community: https://discord.gg/gCyBu8T
+        }
+    }
+}
